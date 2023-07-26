6b9ba9766c37f5ad9472c396671301b990225385
sjtommylee <sjtommylee@gmail.com>
2023-07-26 13:38:39 -0700

    chore: commit save point

--- packages/api/src/trpc.ts
+++ packages/api/src/trpc.ts
@@ -2,3 +2,31 @@ import { initTRPC, TRPCError } from '@trpc/server'
 
 import { type Context } from './context'
 import superjson from 'superjson'
+
+const t = initTRPC.context<Context>().create({
+  transformer: superjson,
+  errorFormatter({ shape }) {
+    return shape
+  },
+})
+
+/**
+ * @middlware -  https://trpc.io/docs/middlewares
+ * function that runs before the procedure and modify content.
+ *
+ * @context - object that is passed to every procedure.
+ */
+const isAuthed = t.middleware(({ next, ctx }) => {
+  if (!ctx.user) {
+    throw new TRPCError({ code: 'UNAUTHORIZED', message: 'Not authenticated' })
+  }
+  return next({
+    ctx: {
+      user: ctx.user,
+    },
+  })
+})
+
+export const router = t.router
+export const publicProcedure = t.procedure
+export const protectedProcedure = t.procedure.use(isAuthed)
