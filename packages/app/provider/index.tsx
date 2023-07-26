0a1a407bfdf1ee79d125c7b029b484d6eb53a2b9
sjtommylee <sjtommylee@gmail.com>
2023-07-26 14:00:53 -0700

    client providers for trpc

--- packages/app/provider/index.tsx
+++ packages/app/provider/index.tsx
@@ -3,31 +3,35 @@ import { useColorScheme } from 'react-native'
 
 import { ToastViewport } from './ToastViewport'
 import config from '../tamagui.config'
+import { AuthProvider } from './auth'
+import { TRPCProvider } from './trpc'
 
 export function Provider({ children, ...rest }: Omit<TamaguiProviderProps, 'config'>) {
   const scheme = useColorScheme()
   return (
-    <TamaguiProvider
-      config={config}
-      disableInjectCSS
-      defaultTheme={scheme === 'dark' ? 'dark' : 'light'}
-      {...rest}
-    >
-      <ToastProvider
-        swipeDirection="horizontal"
-        duration={6000}
-        native={
-          [
-            /* uncomment the next line to do native toasts on mobile. NOTE: it'll require you making a dev build and won't work with Expo Go */
-            // 'mobile'
-          ]
-        }
+    <AuthProvider>
+      <TamaguiProvider
+        config={config}
+        disableInjectCSS
+        defaultTheme={scheme === 'dark' ? 'dark' : 'light'}
+        {...rest}
       >
-        {children}
+        <ToastProvider
+          swipeDirection="horizontal"
+          duration={6000}
+          native={
+            [
+              /* uncomment the next line to do native toasts on mobile. NOTE: it'll require you making a dev build and won't work with Expo Go */
+              // 'mobile'
+            ]
+          }
+        >
+          <TRPCProvider>{children}</TRPCProvider>
 
-        <CustomToast />
-        <ToastViewport />
-      </ToastProvider>
-    </TamaguiProvider>
+          <CustomToast />
+          <ToastViewport />
+        </ToastProvider>
+      </TamaguiProvider>
+    </AuthProvider>
   )
 }
